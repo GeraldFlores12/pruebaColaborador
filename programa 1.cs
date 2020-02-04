276df6863e65851264feb1078afae014139e84e3
GeraldFlores12 <60366178+GeraldFlores12@users.noreply.github.com>
2020-02-04 17:19:57 -0600

    Create programa 1.cs
    esta es la primera prueba con un programa

---
+++ programa 1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab3_semana1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ancho = 0, alto = 0, area = 0, perimetro = 0;
+            String entrada = "";
+            sumas ins = new sumas();
+
+            Console.WriteLine("Ingrese el ancho: ");
+            entrada = Console.ReadLine();
+            ancho = Convert.ToDouble(entrada);
+            Console.WriteLine("Ingrese el alto: ");
+            entrada = Console.ReadLine();
+            alto = Convert.ToDouble(entrada);
+
+            ins.calcularArea(ancho,alto);
+            ins.calcularPerimetro(ancho, alto);
+            Console.WriteLine("el Area es: {0}", ins.area);
+            Console.WriteLine("el perimetro es {0}", ins.perimetro);
+            Console.ReadKey();
+        }
+    }
+}
